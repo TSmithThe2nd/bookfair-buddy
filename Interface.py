a0aa1f2cce8d1ce1a16c91e086ac6db6e499ae12
TSmithThe2nd <smity51@gmail.com>
2023-03-12 20:09:52 -0400

    Init class for interface and adds todo actions

---
+++ Interface.py
@@ -0,0 +1,29 @@
+#Todo import classes
+
+
+
+#todo set up interface tabs
+
+    #Create student
+
+    #create teacher
+
+    #Add author
+     #add book
+
+    #Add book to list
+     #teacher
+     #student
+
+    #Veiw reports
+     #Student books
+     #Teacher books
+     #Student books By teacher
+      #by grade
+    #export list
+
+    #admin
+    #delete student
+    #delete book
+    #edit student
+    #edit teacher
