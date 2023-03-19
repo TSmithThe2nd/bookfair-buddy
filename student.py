bbd21d53104c13365256540adcc6ec5d01f1084a
TSmithThe2nd <smity51@gmail.com>
2023-03-19 19:14:24 -0400

    Creates studentHook method.

--- student.py
+++ student.py
@@ -1,49 +1,56 @@
 # import
+import student as student
+
+# varibles
+studentId = None
+first_name = None
+last_name = None
+grade_level = None
+teacher_id = None
+bookId = None
 
 
-#varibles
-studentId=None
-first_name=None
-last_name=None
-grade_level=None
-bookId=None
+def student_hook(f_name, l_name, g_level, t_id):
+    new_student
+    student.first_name = f_name
+    student.last_name = l_name
+    student.grade_level = g_level
+    student.teacher_id = t_id
 
-#create new student
+# create new student
 new_student= f"""
 INSERT INTO student VALUES
-('0','{studentId}','{first_name}','{last_name}','{grade_level}','{bookId})'
+('0',,'{first_name}','{last_name}','{grade_level}','{teacher_id})'
 """
 
-#read student
-show_all_students="""
+# read student
+show_all_students = """
 SELECT *
 FROM student;
 """
 
-
-
-show_student_books=f"""
+show_student_books = f"""
 SELECT title
 FROM student_books
 WHERE student_id="{studentId}"
 """
 
 # update student
-update_student=f"""
+update_student = f"""
 UPDATE student
 SET first_name= '{first_name}', last_name= '{last_name}', grade_level='{grade_level}'
 WHERE id = '{studentId}';"""
 
-add_book_to_student=f"""
+add_book_to_student = f"""
 INSERT INTO student_books VALUES
 ('{studentId}','{bookId}')
 """
 
-#delete
-delete_student=f"""DELETE FROM student
+# delete
+delete_student = f"""DELETE FROM student
 WHERE id = '{studentId}';"""
 
-remove_book_from_student=f"""
+remove_book_from_student = f"""
 DELETE FROM student_books
 WHERE book_id='{bookId}' and student_id='{studentId}'
 """
